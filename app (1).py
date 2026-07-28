710ccf1bd44305d2c13a51c9f747d0290d3b9074
Hardika-crypto <yugti.1706@gmail.com>
2026-07-28 12:25:44 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -18,7 +18,7 @@ from langchain.messages import SystemMessage, HumanMessage
 import numpy as np
 import streamlit as st
 from langchain_community.document_loaders import PyMuPDFLoader
-from PIL import image
+from PIL import Image
 
 #===========API key load=================
 GOOGLE_API_KEY = st.sidebar.text_input("GEMINI_API_KEY",type="password")
